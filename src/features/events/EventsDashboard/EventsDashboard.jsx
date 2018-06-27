aaa1c57030c0b60f51f92b7a2236aedae73d4481
Sue Kinney <suekinney@Sues-MacBook.local>
2018-06-27 10:38:28 -0600

    dusapp

--- src/features/events/EventsDashboard/EventsDashboard.jsx
+++ src/features/events/EventsDashboard/EventsDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
+    date: '2018-03-27',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,7 +32,7 @@ const eventsDashboard = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
+    date: '2018-03-28',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -58,19 +58,33 @@ const eventsDashboard = [
 class EventDashboard extends Component {
   state = {
     events: eventsDashboard,
-    isOpen: false
+    isOpen: false,
+    selectedEvent: null
   };
 
   handleFormOpen = () => {
     this.setState({
+      selectedEvent: null,
       isOpen: true
     });
   };
 
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState({
+      
+    })
+  }
   handleCancel = () => {
     this.setState({
       isOpen: false
     });
+  };
+
+  handleEditEvent = (eventToUpdate) => () => {
+     this.setState({
+       selectedEvent: eventToUpdate,
+       isOpen: true
+     })
   };
 
   handleCreateEvent = (newEvent) => {
@@ -84,10 +98,11 @@ class EventDashboard extends Component {
   }
 
   render() {
+    const {selectedEvent}=this.state;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={this.state.events} />
+          <EventList onEventEdit={this.handleEditEvent} events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button
@@ -95,7 +110,7 @@ class EventDashboard extends Component {
             positive
             content="Create Event"
           />
-          {this.state.isOpen && <EventForm handleCancel={this.handleCancel} createEvent={this.handleCreateEvent} />}
+          {this.state.isOpen && <EventForm selectedEvent={selectedEvent} handleCancel={this.handleCancel} createEvent={this.handleCreateEvent} />}
         </Grid.Column>
       </Grid>
     );
